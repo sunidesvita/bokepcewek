abd7288e34868b1a2ebfd3eaa3164e97c838e791
sunidesvita <162727018+sunidesvita@users.noreply.github.com>
2025-02-19 00:42:30 +0700

    Add files via upload

--- components/video-card.tsx
+++ components/video-card.tsx
@@ -1,9 +1,6 @@
 import { Card, CardContent } from "./ui/card";
-import { humanDuration, naturalTime } from "@/lib/utils";
 
 import { Badge } from "./ui/badge";
-import { CalendarIcon } from "@radix-ui/react-icons";
-import LikeButton from "./like-button";
 import Link from "next/link";
 import React from "react";
 import Thumbnail from "./thumbnail";
@@ -14,22 +11,22 @@ const VideoCard = ({ video }: any) => {
             <div className="relative">
                 <Link
                     href={`/v/${video.file_code}`}
+                    title={`Watch ${video.title}`}
                     >
                 <Thumbnail
-                    single_img={video.single_img}
-                    splash_img={video.splash_img}
+                    thumbnail={video.thumbnail}
                     title={video.title}
                 />
                 </Link>
             </div>
-            <CardContent className="p-1.5">
+            <CardContent className="p-1.5"><h3>
                 <Link
                     href={`/v/${video.file_code}`}
                     className="line-clamp-2 text-sm md:text-md font-semibold hover:text-primary focus:text-primary"
-                    aria-label={`Watch ${video.title}`}
+                    title={`Watch ${video.title}`}
                 >
                     {video.title}
-                </Link>
+                </Link></h3>
             </CardContent>
         </Card>
     );
